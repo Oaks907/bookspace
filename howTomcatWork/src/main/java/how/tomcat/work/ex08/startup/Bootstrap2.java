cc7483d6c733dfda173da2be4e30f26a6db56e70
李海飞 <haifei.li@renren-inc.com>
2018-07-26 08:31:38 +0800

    feat($Tomcat): ex08:添加载入器

--- howTomcatWork/src/main/java/how/tomcat/work/ex08/startup/Bootstrap2.java
+++ howTomcatWork/src/main/java/how/tomcat/work/ex08/startup/Bootstrap2.java
@@ -1,22 +1,19 @@
 package how.tomcat.work.ex08.startup;
 
-import how.tomcat.work.ex05.valves.ClientIPLoggerValve;
-import how.tomcat.work.ex05.valves.HeaderLoggerValve;
-import how.tomcat.work.ex08.core.SimpleContext;
-import how.tomcat.work.ex08.core.SimpleContextLifecycleListener;
-import how.tomcat.work.ex08.core.SimpleContextMapper;
-import how.tomcat.work.ex08.core.SimpleLoader;
+import how.tomcat.work.ex08.core.SimpleContextConfig;
 import how.tomcat.work.ex08.core.SimpleWrapper;
+import org.apache.catalina.Connector;
 import org.apache.catalina.Context;
 import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleListener;
 import org.apache.catalina.Loader;
-import org.apache.catalina.Mapper;
-import org.apache.catalina.Pipeline;
-import org.apache.catalina.Valve;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.connector.http.HttpConnector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.loader.WebappClassLoader;
+import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.logger.FileLogger;
+import org.apache.naming.resources.ProxyDirContext;
 
 /**
  * Create by haifei on 20/7/2018.
@@ -24,6 +21,7 @@ import org.apache.catalina.logger.FileLogger;
 public class Bootstrap2 {
 
   public static void main(String[] args) {
+    System.setProperty("catalina.base", System.getProperty("user.dir"));
     HttpConnector httpConnector = new HttpConnector();
     Wrapper wrapper1 = new SimpleWrapper();
     wrapper1.setName("Primitive");
@@ -32,37 +30,23 @@ public class Bootstrap2 {
     wrapper2.setName("Modern");
     wrapper2.setServletClass("ModernServlet");
 
-    Context context = new SimpleContext();
+    Context context = new StandardContext();
+    context.setPath("/myApp");
+    context.setDocBase("myApp");
+
+
     context.addChild(wrapper1);
     context.addChild(wrapper2);
 
-    Valve valve1 = new HeaderLoggerValve();
-    Valve valve2 = new ClientIPLoggerValve();
-
-    ((Pipeline) context).addValve(valve1);
-    ((Pipeline) context).addValve(valve2);
-
-    Mapper mapper = new SimpleContextMapper();
-    mapper.setProtocol("http");
-
-    LifecycleListener listener = new SimpleContextLifecycleListener();
-    ((Lifecycle) context).addLifecycleListener(listener);
-
-    context.addMapper(mapper);
-    Loader loader = new SimpleLoader();
-    context.setLoader(loader);
     context.addServletMapping("/Primitive", "Primitive");
     context.addServletMapping("/Modern", "Modern");
 
-    // ------ add logger ----------
-    System.setProperty("catalina.base", System.getProperty("user.dir"));
-    FileLogger logger = new FileLogger();
-    logger.setPrefix("FileLog_");
-    logger.setSuffix(".txt");
-    logger.setTimestamp(true);
-    logger.setDirectory("webroot");
-    context.setLogger(logger);
-    //--------------------------------
+    LifecycleListener listener = new SimpleContextConfig();
+    ((Lifecycle) context).addLifecycleListener(listener);
+
+
+    Loader loader = new WebappLoader();
+    context.setLoader(loader);
 
     httpConnector.setContainer(context);
 
@@ -70,6 +54,14 @@ public class Bootstrap2 {
       httpConnector.initialize();
       ((Lifecycle) httpConnector).start();
       ((Lifecycle) context).start();
+
+
+      WebappClassLoader classLoader = (WebappClassLoader) loader.getClassLoader();
+      System.out.println("Resources' docBase: " + ((ProxyDirContext)classLoader.getResources()).getDocBase());
+      String[] repositories = classLoader.findRepositories();
+      for (int i = 0; i < repositories.length; i++) {
+        System.out.println(" repository: " + repositories[i]);
+      }
 
       System.in.read();
       ((Lifecycle) context).stop();
