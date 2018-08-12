f8a5daaea140d6636c98e795471d0d7eb183951c
李海飞 <haifei.li@renren-inc.com>
2018-08-12 18:33:21 +0800

    feat($Tomcat): standardWrapper

---
+++ howTomcatWork/src/main/java/how/tomcat/work/ex11/core/SimpleContextConfig.java
@@ -0,0 +1,20 @@
+package how.tomcat.work.ex11.core;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+/**
+ * Create by haifei on 12/8/2018.
+ */
+public class SimpleContextConfig implements LifecycleListener {
+
+  public void lifecycleEvent(LifecycleEvent event) {
+    if (Lifecycle.START_EVENT.equals(event.getType())) {
+      Context context = (Context) event.getLifecycle();
+      context.setConfigured(true);
+    }
+  }
+
+}
