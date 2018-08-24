beb1e4546daf72ca102ebdfbf5c7acd9417f5b63
李海飞 <haifei.li@renren-inc.com>
2018-08-24 15:53:29 +0800

    feat($Spring Boot): ch3 spring Aware 利用bean使用spring资源

---
+++ springbootInAction/highlig_spring4/src/main/java/com/wisely/highlight_spring4/ch3/aware/AwareConfig.java
@@ -0,0 +1,12 @@
+package com.wisely.highlight_spring4.ch3.aware;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Create by haifei on 24/8/2018.
+ */
+@Configuration
+@ComponentScan("com.wisely.highlight_spring4.ch3.aware")
+public class AwareConfig {
+}
