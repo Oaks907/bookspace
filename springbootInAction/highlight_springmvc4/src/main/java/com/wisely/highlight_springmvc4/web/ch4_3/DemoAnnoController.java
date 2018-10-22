119856097fdb29d99346ba2e968c14c7b2be5922
李海飞 <haifei.li@renren-inc.com>
2018-10-22 10:20:11 +0800

    feat($SpringBoot): 修复Github报的high serverity问题

--- springbootInAction/highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/web/ch4_3/DemoAnnoController.java
+++ springbootInAction/highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/web/ch4_3/DemoAnnoController.java
@@ -1,13 +1,10 @@
 package com.wisely.highlight_springmvc4.web.ch4_3;
 
-import com.sun.xml.internal.ws.resources.HttpserverMessages;
 import com.wisely.highlight_springmvc4.Domain.DemoObj;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.HttpServerErrorException;
 
 import javax.servlet.http.HttpServletRequest;
 
