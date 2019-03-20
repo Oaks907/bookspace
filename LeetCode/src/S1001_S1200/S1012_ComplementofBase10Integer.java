916ef8330a1be331e07720926c4bba11a0e3e711
李海飞 <haifei.li@renren-inc.com>
2019-03-20 10:53:30 +0800

    feat($Leetcode): S1012 求一个数的补码

---
+++ LeetCode/src/S1001_S1200/S1012_ComplementofBase10Integer.java
@@ -0,0 +1,32 @@
+package S1001_S1200;
+
+import com.sun.org.apache.regexp.internal.REUtil;
+
+import static java.lang.Integer.toBinaryString;
+
+/**
+ * Create by haifei on 19/3/2019 7:38 PM.
+ * https://leetcode.com/problems/complement-of-base-10-integer/
+ */
+public class S1012_ComplementofBase10Integer {
+
+  public int bitwiseComplement(int N) {
+    int n = 1;
+
+    while (N > n) {
+      n = 2 * n + 1;
+    }
+
+    return N ^ n;
+  }
+
+  public static void main(String[] args) {
+
+    final S1012_ComplementofBase10Integer base10Integer =
+      new S1012_ComplementofBase10Integer();
+
+    System.out.println(base10Integer.bitwiseComplement(5)); //2
+    System.out.println(base10Integer.bitwiseComplement(7)); //0
+    System.out.println(base10Integer.bitwiseComplement(10)); //5
+  }
+}
