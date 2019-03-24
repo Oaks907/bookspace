954eff146856c8c798e469a7c239d574593b8234
李海飞 <haifei.li@renren-inc.com>
2019-03-24 17:54:58 +0800

    feat($OS): 初始化TSS

--- osInAction/c11_process/a/kernel/init.c
+++ osInAction/c11_process/a/kernel/init.c
@@ -5,6 +5,8 @@
 #include "memory.h"
 #include "thread.h"
 #include "console.h"
+#include "keyboard.h"
+#include "tss.h"
 
 /*负责初始化所有模块 */
 void init_all() {
@@ -15,4 +17,5 @@ void init_all() {
    	timer_init();  // 初始化PIT
 	console_init(); //控制台初始化最好放在开中断之前
 	keyboard_init();  // 键盘初始化
+    tss_init();       // tss初始化
 }
