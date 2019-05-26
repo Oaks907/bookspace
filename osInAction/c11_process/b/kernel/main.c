7942937d26d5792a0142ee3a7bdc3272b82ddfd8
李海飞 <haifei.li@renren-inc.com>
2019-05-26 23:50:02 +0800

    feat($OS): c11 b 实现用户进程

--- osInAction/c11_process/b/kernel/main.c
+++ osInAction/c11_process/b/kernel/main.c
@@ -3,21 +3,25 @@
 #include "thread.h"
 #include "interrupt.h"
 #include "console.h"
+#include "process.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
+void u_prog_a(void);
+void u_prog_b(void);
+int test_var_a = 0; test_var_b = 0;
 
 int  main(void) {
 	put_str("I am kernel\n");
 	init_all();
 
-//	thread_start("k_thread_a", 31, k_thread_a, "argA ");
-//	thread_start("k_thread_b", 8, k_thread_b, "argB ");	
+	thread_start("k_thread_a", 31, k_thread_a, "argA ");
+	thread_start("k_thread_b", 31, k_thread_b, "argB ");	
+	process_execute(u_prog_a, "user_prog_a");
+	process_execute(u_prog_b, "user_prog_b");
 
 	intr_enable();	//打开中断,使时钟中断起作用
-	while(1); //{
-		//console_put_str("Main ");
-	//};
+	while(1); 
 	return 0;
 }
 
@@ -37,4 +41,17 @@ void k_thread_b(void* arg){
 	while(1) {
 	  console_put_str(para);
 	}
+}
+/* 测试用户进程 */
+void u_prog_a(void) {
+   while(1) {
+      test_var_a++;
+   }
+}
+
+/* 测试用户进程 */
+void u_prog_b(void) {
+   while(1) {
+      test_var_b++;
+   }
 }
