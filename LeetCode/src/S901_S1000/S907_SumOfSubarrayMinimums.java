ab8ff86816aaf93f1f7444aa3b0d93f2c922e075
李海飞 <haifei.li@renren-inc.com>
2019-12-13 12:54:52 +0800

    feat($Leetcode): Array
    S907_SumOfSubarrayMinimums.java

--- LeetCode/src/S901_S1000/S907_SumOfSubarrayMinimums.java
+++ LeetCode/src/S901_S1000/S907_SumOfSubarrayMinimums.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * Create by haifei on 11/12/2019 12:36 PM.
@@ -50,31 +51,68 @@ public class S907_SumOfSubarrayMinimums {
     for (int i = 0; i < A.length; i++) {
       while (!stack.isEmpty() && stack.peek()[0] > A[i]) stack.pop();
       prevLess[i] = stack.isEmpty() ? -1 : stack.peek()[1];
-      stack.push(new int[] {A[i], i});
+      stack.push(new int[]{A[i], i});
     }
     // calc next less
     stack.clear();
     for (int i = A.length - 1; i >= 0; i--) {
       while (!stack.isEmpty() && stack.peek()[0] >= A[i]) stack.pop();
       nextLess[i] = stack.isEmpty() ? A.length : stack.peek()[1];
-      stack.push(new int[] {A[i], i});
+      stack.push(new int[]{A[i], i});
     }
     // calc result
     int res = 0;
-    int mod = (int)1e9 + 7;
+    int mod = (int) 1e9 + 7;
     for (int i = 0; i < A.length; i++) {
       res = (res + A[i] * (i - prevLess[i]) * (nextLess[i] - i)) % mod;
     }
     return res;
   }
 
+  public int sumSubarrayMins3(int[] A) {
+    int res = 0, n = A.length, mod = (int) 1e9 + 7;
+    int[] left = new int[n], right = new int[n];
+    Stack<int[]> s1 = new Stack<>(), s2 = new Stack<>();
+
+    for (int i = 0; i < A.length; i++) {
+      int count = 1;
+
+      while (!s1.isEmpty() && s1.peek()[0] > A[i]) {
+        count += s1.pop()[1];
+      }
+
+      s1.push(new int[]{A[i], count});
+      left[i] = count;
+    }
+
+    for (int i = A.length - 1; i >= 0; i--) {
+      int count = 1;
+
+      while (!s2.isEmpty() && s2.peek()[0] < A[i]) {
+        count += s2.pop()[1];
+      }
+
+      s2.push(new int[]{A[i], count});
+      right[i] = count;
+    }
+
+    for (int i = 0; i < A.length; i++) {
+      res = (res + A[i] * left[i] * right[i]) % mod;
+    }
+
+    return res;
+  }
 
 
   @Test
   public void test() {
     int[] A = {3, 1, 2, 4};
 
-    int result = sumSubarrayMins(A);
+    int result = sumSubarrayMins2(A);
+
+    Assert.assertEquals(17, result);
+
+    result = sumSubarrayMins3(A);
 
     Assert.assertEquals(17, result);
   }
